a78c427ecddb4f546b24b5ca7ae2aa7d52c094fa
Anders Kleimark <anders.kleimark@proton.me>
2026-06-24 12:11:03 +0200

    Tillägg av funktioner samt ny dokumentation.

--- database/database.py
+++ database/database.py
@@ -1,16 +1,22 @@
 import sqlite3
 import os
 
+# Klass för att hantera databasen (sqlite).
+
 
 class Database:
 
+    DATABASE_NAME = "stryktips.db"
+
     def __init__(self):
-        database_exists = os.path.exists("stryktips.db")
-        self.conn = sqlite3.connect("stryktips.db")
+        database_exists = os.path.exists(self.DATABASE_NAME)
+        self.conn = sqlite3.connect(self.DATABASE_NAME)
         self.conn.execute("PRAGMA foreign_keys = ON")
         self.cursor = self.conn.cursor()
         if not database_exists:
             self.create_database_tables()
+
+    # Funktion som skapar databastabellerna.
 
     def create_database_tables(self):
         print("create_database_tables")
@@ -53,13 +59,18 @@ class Database:
 
         self.conn.commit()
 
+    # Funktion som lagrar en tipskupong för år 'year' och vecka 'week' i databasen.
+    # Funktionen returnerar det rad-id som aktualiseras för kupongen.
     def create_coupon(self, year, week):
         self.cursor.execute("""
-            INSERT OR IGNORE INTO coupons(year, week)
-            VALUES (?, ?)
+        INSERT INTO coupons(year, week)
+        VALUES (?, ?)
         """, (year, week))
-        self.conn.commit()
 
+        self.conn.commit()
+        return self.cursor.lastrowid
+
+    # Funktion som returnerar den tipskupong för år=year och månad=week.
     def get_coupon(self, year, week):
         self.cursor.execute("""
             SELECT id, year, week
@@ -68,6 +79,24 @@ class Database:
         """, (year, week))
         return self.cursor.fetchone()
 
+    # Funktion som lägger till en tipsmatch i databasen.
+    def add_match(self, coupon_id, match_number, home_team, away_team):
+        self.cursor.execute("""
+        INSERT INTO matches(
+            coupon_id,
+            match_number,
+            home_team,
+            away_team
+        )
+        VALUES (?, ?, ?, ?)
+        """, (
+            coupon_id,
+            match_number,
+            home_team,
+            away_team
+        ))
+
+    # Funktion som returnerar alla matcher för en viss tipskupong.
     def get_matches(self, coupon_id):
         self.cursor.execute("""
             SELECT match_number, home_team, away_team
@@ -77,6 +106,7 @@ class Database:
         """, (coupon_id,))
         return self.cursor.fetchall()
 
+    # Funktion som lagrar ett matchresultat i databasen.
     def set_result(self, match_id, result):
         self.cursor.execute("""
             INSERT INTO results(match_id, result)
@@ -85,5 +115,6 @@ class Database:
         """, (match_id, result))
         self.conn.commit()
 
+    # Funktion som stänger ner databasanslutningen.
     def close(self):
         self.conn.close()
