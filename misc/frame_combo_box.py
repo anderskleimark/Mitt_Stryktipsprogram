458eccfeca81d6936969fc5d1c0ea62516e30a09
Anders Kleimark <anders.kleimark@proton.me>
2026-07-12 20:35:13 +0200

    Tillägg av en ny klass för att kunna ändra ramen via combo-boxar.

---
+++ misc/frame_combo_box.py
@@ -0,0 +1,20 @@
+from misc.base_combo_box import BaseComboBox
+
+# Klass som ärver BaseComboBox. Tanken med klassen är att skapa combo-boxar, där användaren bara kan välja legala varianter av tipstecken i ramen för de olika systemen.
+
+
+class FrameComboBox(BaseComboBox):
+
+    def __init__(self):
+        super().__init__()
+
+        self.addItems([
+            "",
+            "1",
+            "X",
+            "2",
+            "1X",
+            "12",
+            "X2",
+            "1X2"
+        ])
