ae11ef19de7e5da663cc6fa6f4a873dae831512e
Anders Kleimark <anders.kleimark@proton.me>
2026-07-31 18:28:56 +0200

    Flytt av instansen av EntityFactory till Repository.

--- database/repositories/repository.py
+++ database/repositories/repository.py
@@ -1,6 +1,9 @@
+from database.factories.entity_factory import EntityFactory
+
 
 class Repository:
     def __init__(self, database):
         self.database = database
         self.connection = database.connection
         self.cursor = database.cursor
+        self.factory = EntityFactory()
