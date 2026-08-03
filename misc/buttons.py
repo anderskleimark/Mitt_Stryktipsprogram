e2a653e27e62dee07c356194398c6830a81a161b
Anders Kleimark <anders.kleimark@proton.me>
2026-08-03 18:33:36 +0200

    En ny knapp med egen stil kan nu användas.

--- misc/buttons.py
+++ misc/buttons.py
@@ -31,16 +31,33 @@ class BaseButton(QPushButton):
             Bygger och applicerar knappens stylesheet utifrån
             aktuella stilinställningar.
         """
-        css = ""
+
+        normal_css = ""
+        disabled_css = ""
 
         for key, value in self._style.items():
-            css += f"{key}: {value};\n"
 
-        self.setStyleSheet(f"""
+            if key.startswith("disabled-"):
+                css_key = key.replace("disabled-", "")
+                disabled_css += f"{css_key}: {value};\n"
+
+            else:
+                normal_css += f"{key}: {value};\n"
+
+        stylesheet = f"""
             QPushButton {{
-                {css}
+                {normal_css}
             }}
-        """)
+        """
+
+        if disabled_css:
+            stylesheet += f"""
+                QPushButton:disabled {{
+                    {disabled_css}
+                }}
+            """
+
+        self.setStyleSheet(stylesheet)
 
     def set_style_value(self, key, value):
         """
@@ -57,7 +74,39 @@ class AddButton(BaseButton):
         super().__init__("Lägg till", parent)
 
     def default_style(self):
-        return Style.BUTTON
+        return Style.BUTTON.copy()
+
+
+class EditButton(BaseButton):
+    def __init__(self, parent=None):
+        super().__init__("Redigera", parent)
+
+    def default_style(self):
+        return Style.BUTTON.copy()
+
+
+class DeleteButton(BaseButton):
+    def __init__(self, parent=None):
+        super().__init__("Radera", parent)
+
+    def default_style(self):
+        return Style.DELETE_BUTTON.copy()
+
+
+class BackButton(BaseButton):
+    def __init__(self, parent=None):
+        super().__init__("Tillbaka", parent)
+
+    def default_style(self):
+        return Style.BUTTON.copy()
+
+
+class ShowTableButton(BaseButton):
+    def __init__(self, parent=None):
+        super().__init__("Visa tabell", parent)
+
+    def default_style(self):
+        return Style.BUTTON.copy()
 
 
 class InfoButton(BaseButton):
@@ -65,16 +114,4 @@ class InfoButton(BaseButton):
         super().__init__("Visa information", parent)
 
     def default_style(self):
-        return Style.BUTTON
-
-
-class DeleteButton(BaseButton):
-    def __init__(self, parent=None):
-        super().__init__("Radera", parent)
-        self.setProperty(
-            "buttonClass",
-            "warning"
-        )
-
-    def default_style(self):
-        return Style.BUTTON
+        return Style.BUTTON.copy()
--- misc/styles.py
+++ misc/styles.py
@@ -1,8 +1,18 @@
 class Style:
     BUTTON = {
+        # Storlek
         "padding": "6px 12px",
         "margin": "0px",
         "font-size": "11pt",
         "min-height": "30px",
-        "max-height": "120px"
+        "max-height": "120px",
+
+    }
+    DELETE_BUTTON = {
+        **BUTTON,
+        "background-color": "#C62828",
+        "color": "white",
+
+        "disabled-background-color": "#A0A0A0",
+        "disabled-color": "#E0E0E0"
     }
